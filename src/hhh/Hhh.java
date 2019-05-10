6dffb25af7c995d75e1a16e4da7dc3c8a49c6596
m <m@g>
2019-05-10 13:27:18 +0200

    version1

---
+++ src/hhh/Hhh.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hhh;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Hhh {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int colum=8;
+        System.out.println(colum);
+    }
+    
+}
